3d90ac50ea916d6710f1afbb9eac7a7ea63c25d8
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-09 18:25:37 +0530

    improved in time complexity wise

--- Java/Primeornot.java
+++ Java/Primeornot.java
@@ -1,24 +1,25 @@
 package prime;
 import java.util.Scanner;
-public class Primeornot {
-	public static void main(String args[]) {
-	int flag = 0;
-	Scanner sc = new Scanner(System.in);
-	int n = sc.nextInt();
-	for(int i=2;i<n-1;i++)
-	{
-		if(n%i==0)
-		{
-			flag = flag +1;
-		}
-	}
-	if(flag==0)
-	{
-		System.out.println(n+ " is prime");
-	}
-	else
-	{
-		System.out.println(n+" is not prime");
-	}
-	}
+public class Primeornot 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc =new Scanner(System.in);
+	boolean prime=isPrime(sc.nextInt());
+	System.out.print(prime?"Prime Number":"not a Prime Number");    
+    }
+		
+    static boolean isPrime(int number)
+    {	    	
+      if(number<=1)
+  	 return false;
+      int c=2;
+      while(c*c<=number)
+      {
+	 if(number%c==0)
+	     return false;
+         c++;
+      }
+      return true;
+      }
 }
